8903b57069a7f2720bd5b2531010480771231cea
DevScyu <scyu@scyu.dev>
2026-05-24 12:50:02 +0100

    feat: add reka-ui with nuxt auto-imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/global.css'],
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'reka-ui/nuxt'],
   eslint: {
     config: {
       standalone: false,
